bdc60f7ea8c43cfe8a74aa19b473716aa4c306bd
Simone <simone-preite_89@hotmail.it>
2018-06-30 18:03:46 +0200

    testing environment

--- source/main.java
+++ source/main.java
@@ -1,43 +1,59 @@
 import ast.*;
-import grammars.FOOLLexer;
-import grammars.FOOLParser;
+import grammars.FOOL.FOOLLexer;
+import grammars.FOOL.FOOLLexer.*;
+import grammars.FOOL.FOOLParser;
+import grammars.FOOL.FOOLVisitorImpl;
 
 
 import org.antlr.v4.runtime.*;
 
-public class main{
+import java.io.File;
 
-private static Node lexicalAndSyntacticAnalysis(CharStream input) throws LexerException {
+public class main{
+	
+	private static Node lexicalAndSyntacticAnalysis(CharStream input) /*throws LexerException*/ {
 		FOOLLexer lexer = new FOOLLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		if (lexer.LexicalErrors > 0) {
-		throw new LexerException(lexer.errors);
+		if (lexer.lexicalErrors > 0) {
+			System.out.println("Lexer errors");
+			//throw new LexerException(lexer.errors);
 		}
 		FOOLParser parser = new FOOLParser(tokens);
-		FoolVisitorImpl visitor = new FoolVisitorImpl();
-		return visitor.visit(parser.prog()); //generazione AST
-		}
-
-
-public static String run(CharStream input) {
-		CodegenUtils.reset();
-		String result = "";
-		try {
+		FOOLVisitorImpl visitor = new FOOLVisitorImpl();
 		
-		if (enableLogging) {
+		FOOLParser.ProgContext res = parser.prog();
+		
+		return visitor.visit(res); //generazione AST
+	}
+	
+	
+	public static String run(CharStream input) {
+		String result = "";
+		
 		System.out.println("Lexer & parser...");
-		}
+		
 		
 		Node ast = lexicalAndSyntacticAnalysis(input);
 		
+		System.out.println("Visualizing AST...");
+		System.out.println(ast.toPrint(""));
 		
 		return result;
-		}
-
-public static void main(String[] args) throws Exception{
-		String filename="input.fool";
-		CharStream input=CharStreams.fromFileName(filename);
-		run(input);
-		System.out.println(output);
-		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		
+		File inputFile = new File("code/input.fool");
+		CharStream input=CharStreams.fromFileName(inputFile.getAbsolutePath());
+		//String output = run(input);
+		System.out.println("Lexer & parser...");
+		
+		
+		Node ast = lexicalAndSyntacticAnalysis(input);
+		
+		System.out.println("Visualizing AST...");
+		String res = ast.toPrint("");
+		System.out.println(res);
+		
+	}
 }
